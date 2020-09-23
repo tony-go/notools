04697cb343debc3d2e80abded5d00519ea988c15
Tony Gorez <gorez.tony@gmail.com>
2020-09-23 12:17:06 +0200

    copy redirect with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
+const CopyPlugin = require("copy-webpack-plugin")
 const outputDir = path.join(__dirname, "build/")
 
 const isProd = process.env.NODE_ENV === "production"
@@ -13,6 +14,13 @@ module.exports = {
     filename: "Index.js"
   },
   plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'public/_redirects'),
+        },
+      ],
+    }),
     new HtmlWebpackPlugin({
       template: "public/index.html",
       inject: false
